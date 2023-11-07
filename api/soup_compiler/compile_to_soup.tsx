6e63fb7d999ce052919bac7e47aae226e1af2f1f
seveibar <seveibar@gmail.com>
2023-11-06 21:42:40 -0800

    wip

--- api/soup_compiler/compile_to_soup.tsx
+++ api/soup_compiler/compile_to_soup.tsx
@@ -24,26 +24,47 @@ export default async (req: Request) => {
 
   // const availablePort = 4000 + Math.floor(Math.random() * 1000)
 
+  // Add a file that does the thing
+  typescript_filesystem["__ENTRYPOINT__.tsx"] = `
+
+// TODO dynamic import so we can check for error more easily
+import { ${target_export} } from "./${target_filepath}"
+
+console.log("we did it!", ${target_export})
+
+
+
+  `.trim()
+
   // create a temporary directory representing the filesystem
-  // const testDir = path.join(
-  //   "./unsafe-usercode",
-  //   `dump_${Math.random().toString(32).slice(2)}`
-  // )
+  const testDir = path.join(
+    "./unsafe-usercode",
+    `dump_${Math.random().toString(32).slice(2)}`
+  )
+  console.log(testDir)
 
-  // fs.mkdirSync(testDir, {
-  //   recursive: true,
-  // })
+  fs.mkdirSync(testDir, {
+    recursive: true,
+  })
 
-  // // create the files from the filesystem config
-  // for (const [filepath, contents] of Object.entries(typescript_filesystem)) {
-  //   const fullPath = path.join(testDir, filepath)
-  //   fs.mkdirSync(path.dirname(fullPath), { recursive: true })
-  //   fs.writeFileSync(fullPath, contents)
-  // }
+  // create the files from the filesystem config
+  for (const [filepath, contents] of Object.entries(typescript_filesystem)) {
+    const fullPath = path.join(testDir, filepath)
+    fs.mkdirSync(path.dirname(fullPath), { recursive: true })
+    fs.writeFileSync(fullPath, contents)
+  }
 
-  const transpiler = new Bun.Transpiler()
-  console.log("running transpiler...")
-  console.log(transpiler.transformSync(typescript_filesystem[target_filepath]))
+  const out = await Bun.build({
+    root: testDir,
+    entrypoints: [path.join(testDir, "__ENTRYPOINT__.tsx")],
+    outdir: path.join(testDir, "__OUTPUT__"),
+  })
+
+  const evalResult = eval(fs.readFileSync(out.outputs[0].path, "utf-8"))
+
+  // const transpiler = new Bun.Transpiler()
+  // console.log("running transpiler...")
+  // console.log(transpiler.transformSync(typescript_filesystem[target_filepath]))
   // console.log(testDir)
   // const importedFile = await import(`./${testDir}/${target_filepath}`)
 
