f2bf81da37ea37b56fcd8e4103441690b4e2fa2a
seveibar <seveibar@gmail.com>
2023-11-07 20:36:53 -0800

    fix: add version command

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import { startServer } from "."
 import minimist from "minimist"
+import packageJson from "./package.json"
 
 const args = minimist(Bun.argv)
+
+if (args.version) {
+  console.log(`tsci-compiler v${packageJson.version}`)
+  process.exit(0)
+}
 
 const port = args.port ? parseInt(args.port) : 3000
 
