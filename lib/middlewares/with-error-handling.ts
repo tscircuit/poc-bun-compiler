0a28cc1e9c08d59bd4e2e47035321ad4fe17e8ad
seveibar <seveibar@gmail.com>
2023-11-11 23:10:13 -0800

    fix: middleware should use modern request

--- lib/middlewares/with-error-handling.ts
+++ lib/middlewares/with-error-handling.ts
@@ -1,13 +1,21 @@
-export const withErrorHandling = (next) => async (req, res) => {
+export const withErrorHandling = (next) => async (req) => {
   try {
-    return await next(res, res)
-  } catch (e) {
+    return await next(req)
+  } catch (e: any) {
     console.log(`Error while processing request: ${e.toString()}`)
-    res.status(500).json({
-      error: {
-        error_type: "internal_server_error",
-        message: e.toString(),
-      },
-    })
+    return new Response(
+      JSON.stringify({
+        error: {
+          error_type: "internal_server_error",
+          message: e.toString(),
+        },
+      }),
+      {
+        status: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    )
   }
 }
